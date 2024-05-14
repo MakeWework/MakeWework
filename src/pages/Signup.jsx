772c36b9438c862dc8005d26c9b189f2297126eb
Dennar David <dennardavid@gmail.com>
2024-05-14 16:35:58 +0100

    sign up modifications

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,74 +1,80 @@
-import {useState} from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 function SignUp() {
+  const cardOriginalColor = "rgba(255, 255, 255, 0.6)";
+  const cardClicked = "rgba(40, 167, 70, 0.6)";
 
-  const [freelancerColor, setFreelancerColor] = useState('#28a74636'); 
-  const [clientColor, setClientColor]= useState('#28a74636');
+  const [freelancerColor, setFreelancerColor] = useState(cardOriginalColor);
+  const [clientColor, setClientColor] = useState(cardOriginalColor);
 
   const handleFreelancerClick = () => {
-    setFreelancerColor(freelancerColor === '#28a74636' ? '#28a746' : '#28a74636');
-    setClientColor('#28a74636');
+    setFreelancerColor(
+      freelancerColor === cardOriginalColor ? cardClicked : cardOriginalColor
+    );
+    setClientColor(cardOriginalColor);
   };
   const handleClientClick = () => {
-    setClientColor(clientColor === '#28a74636' ? '#28a746' : '#28a74636');
-    setFreelancerColor('#28a74636');
+    setClientColor(
+      clientColor === cardOriginalColor ? cardClicked : cardOriginalColor
+    );
+    setFreelancerColor(cardOriginalColor);
   };
-  
-    return (
-      <div>
-        <Navbar/>
-        <div id="signup">
-          
-          <div id="signup-container">
-            <div id= "usertype">
-              
-              <h3>Looking for a job?...</h3>
 
-              <h3> Or searching for the right fit for the job?</h3>
+  return (
+    <>
+      <Navbar />
+      <section id="signup">
+        <div id="signup-container">
+          <div id="usertype">
+            <h3>Looking for a job?...</h3>
 
-              <p id="role">Please select your role</p>
+            <h3> Or searching for the right fit for the job?</h3>
 
-              <div id= "freelancercard" style={{ backgroundColor: freelancerColor }} onClick={handleFreelancerClick}>
+            <p id="role">Please select your role</p>
 
-                    <div id= "freelancerPic">
+            {/* Freelancer card */}
+            <div
+              id="freelancercard"
+              style={{
+                backgroundColor: freelancerColor,
+              }}
+              onClick={handleFreelancerClick}
+            >
+              <div id="freelancerPic"></div>
 
-                    </div>
-
-                    <p>I am a freelancer looking for jobs</p>        
-
-              </div>
-
-              <div id= "clientcard" style={{ backgroundColor: clientColor }} onClick={handleClientClick}>
-
-                    <div id= "clientPic"></div>
-
-                    <p>I am a client looking for talent</p>        
-
-              </div>
-                
-              <Link to= "/create_acct"><button id= "createacct">Create an account</button></Link>
-              
-              
-                
-                  
-                
-                
-
-              <p id="have-acct">Already have an account? <a href="/">LOG IN</a></p>
-
+              <p>I am a freelancer looking for jobs</p>
             </div>
 
+            {/* Client Card */}
+            <div
+              id="clientcard"
+              style={{
+                backgroundColor: clientColor,
+              }}
+              onClick={handleClientClick}
+            >
+              <div id="clientPic"></div>
+
+              <p>I am a client looking for talent</p>
+            </div>
+            <div className="create-btn">
+              <Link to="/create_acct">
+                <button id="createacct">Create an account</button>
+              </Link>
+            </div>
+
+            <p id="have-acct">
+              Already have an account?{" "}
+              <Link to="/login" className="Link">
+                LOG IN
+              </Link>
+            </p>
           </div>
-          
-
-
-          
-          
         </div>
-      </div>
-        
-    );
-  }
-  
-  export default SignUp;
+      </section>
+    </>
+  );
+}
+
+export default SignUp;
