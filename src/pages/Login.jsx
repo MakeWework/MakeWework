479f7fb31a577f5e12f95807906c54e4b46dc761
Mbiatke Nkanta <mbiatke.nkanta@gmail.com>
2024-05-15 14:23:34 +0100

    NkantaMbiatke

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
       <Navbar />
       <form action="">
         <h1>Hello !</h1>
-        <h2>Come Make We Work</h2>
+        <h2>Come Make We Work!!</h2>
         <div className="input-box">
           <input type="text" placeholder="Username" required />
         </div>
@@ -18,7 +18,10 @@ function Login() {
         <div className="forgot-password">
           <a href="#">Forgot password?</a>
         </div>
-        <button type="submit">Login</button>
+        <button className="login" type="submit">Login</button>
+
+        <h3>OR</h3>
+        <button className="signup">Sign Up With Google</button>
       </form>
     </div>
   );
